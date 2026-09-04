c281d414e534032b9aa888c26684a3e6dc7fe0f4
harrysbroom <harrysbroom@gmail.com>
2026-09-03 19:39:04 -0500

    Update custom-font.js

--- custom-font.js
+++ custom-font.js
@@ -1,7 +1,18 @@
 (function () {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = "https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,400;0,7..72,600;1,7..72,400&display=swap";
+  document.head.appendChild(link);
+
   const css = `
+    @font-face {
+      font-family: TMBook;
+      src: local("Bookerly"), local("Bookerly Regular"), local("Bookerly-Regular");
+      font-weight: 400;
+      font-style: normal;
+    }
     html, body, #__next, #__next *:not(code):not(pre):not(.font-mono):not(.font-mono *) {
-      font-family: Bookerly, "Bookerly Regular", "Bookerly-Regular", serif !important;
+      font-family: TMBook, Literata, serif !important;
     }
     code, pre, .font-mono, .font-mono * {
       font-family: ui-monospace, Menlo, Consolas, monospace !important;
